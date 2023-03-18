0ae25bc8bf376d77689bff910184aae575aabef1
Frank Bodholdt Jakobsen <frankbj@bbsyd.dk>
2023-03-18 16:56:04 +0100

    Cleaned up and reduced to 2 parallel methods Set correct compile options

--- FractalFramework/FractalFramework.cpp
+++ FractalFramework/FractalFramework.cpp
@@ -79,16 +79,16 @@
 
 constexpr int nMaxThreads = 32;
 
-class olcFractalExplorer : public olc::PixelGameEngine
+class FractalFramework : public olc::PixelGameEngine
 {
 public:
-	olcFractalExplorer()
+	FractalFramework()
 	{
-		sAppName = "Brute Force Processing";
+		sAppName = "Fractal Framework";
 	}
 
 	int* pFractal = nullptr;
-	int nMode = 5;
+	int nMode = 1;
 	int nIterations = 256;
 
 public:
@@ -100,148 +100,14 @@ public:
 		// MS Specific - see std::aligned_alloc for others
 		pFractal = (int*)_aligned_malloc(size_t(ScreenWidth()) * size_t(ScreenHeight()) * sizeof(int), 64);
 
-		InitialiseThreadPool();
 		return true;
 	}
 
 	bool OnUserDestroy() override
 	{
-		// Stop Worker threads
-		for (int i = 0; i < nMaxThreads; i++)
-		{
-			workers[i].alive = false;		 // Allow thread exit
-			workers[i].cvStart.notify_one(); // Fake starting gun
-		}
-
-		// Clean up worker threads
-		for (int i = 0; i < nMaxThreads; i++)
-			workers[i].thread.join();
-
 		// Clean up memory
 		_aligned_free(pFractal);
 		return true;
-	}
-
-	// Method 1) - Super simple, no effort at optimising
-	void CreateFractalBasic(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations)
-	{
-		double x_scale = (frac_br.x - frac_tl.x) / (double(pix_br.x) - double(pix_tl.x));
-		double y_scale = (frac_br.y - frac_tl.y) / (double(pix_br.y) - double(pix_tl.y));
-		
-		for (int y = pix_tl.y; y < pix_br.y; y++)
-		{
-			for (int x = pix_tl.x; x < pix_br.x; x++)
-			{
-				std::complex<double> c(x * x_scale + frac_tl.x, y * y_scale + frac_tl.y);
-				std::complex<double> z(0, 0);
-
-				int n = 0;
-				while (abs(z) < 2.0 && n < iterations)
-				{
-					z = (z * z) + c;
-					n++;
-				}
-
-				pFractal[y * ScreenWidth() + x] = n;
-			}
-		}
-	}
-
-	// Method 2) - Attempt to pre-calculate as much as possible, and reduce
-	// repeated multiplications
-	void CreateFractalPreCalculate(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations)
-	{
-		double x_scale = (frac_br.x - frac_tl.x) / (double(pix_br.x) - double(pix_tl.x));
-		double y_scale = (frac_br.y - frac_tl.y) / (double(pix_br.y) - double(pix_tl.y));
-
-		double x_pos = frac_tl.x;
-		double y_pos = frac_tl.y;
-
-		int y_offset = 0;
-		int row_size = pix_br.x - pix_tl.x;
-
-		int x, y, n;
-		std::complex<double> c, z;
-
-		for (y = pix_tl.y; y < pix_br.y; y++)
-		{
-			x_pos = frac_tl.x;
-			for (x = pix_tl.x; x < pix_br.x; x++)
-			{				
-				c = { x_pos, y_pos };
-				z = { 0,0 };
-
-				n = 0;
-				while (abs(z) < 2.0 && n < iterations)
-				{
-					z = (z * z) + c;
-					n++;
-				}
-
-				pFractal[y_offset + x] = n;
-				x_pos += x_scale;
-			}
-
-			y_pos += y_scale;
-			y_offset += row_size;
-		}
-	}
-
-
-	// Method 3) - Replace std::complex with just hard coded mathematics
-	void CreateFractalNoComplex(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations)
-	{
-		double x_scale = (frac_br.x - frac_tl.x) / (double(pix_br.x) - double(pix_tl.x));
-		double y_scale = (frac_br.y - frac_tl.y) / (double(pix_br.y) - double(pix_tl.y));
-
-		double x_pos = frac_tl.x;
-		double y_pos = frac_tl.y;
-
-		int y_offset = 0;
-		int row_size = ScreenWidth();
-
-		int x, y, n;
-
-		double cr = 0;
-		double ci = 0;
-		double zr = 0;
-		double zi = 0;
-		double re = 0;
-		double im = 0;
-		double zr2 = 0;
-		double zi2 = 0;
-
-		for (y = pix_tl.y; y < pix_br.y; y++)
-		{
-			x_pos = frac_tl.x;
-			ci = y_pos;
-			for (x = pix_tl.x; x < pix_br.x; x++)
-			{
-				cr = x_pos;
-				zr = 0;
-				zi = 0;
-				zr2 = 0;
-				zi2 = 0;
-
-				n = 0;
-				while ((zr2 + zi2) < 4.0 && n < iterations)
-				{
-					re = zr2 - zi2 + cr;
-					im = zr * zi * 2.0 + ci;
-					zr = re;
-					zi = im;
-					zr2 = zr * zr;
-					zi2 = zi * zi;
-					n++;
-				}
-
-				pFractal[y_offset + x] = n;
-				x_pos += x_scale;
-			}
-
-			y_pos += y_scale;
-			y_offset += row_size;
-		}
 	}
 
 	// New parallel method, using OpenMP
@@ -303,82 +169,7 @@ public:
 		}
 	}
 
-	// Old Method 4) - Use AVX2 Vector co-processor to handle 4 fractal locations at once - not available on my home PC
-	void CreateFractalIntrinsics(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations)
-	{
-		double x_scale = (frac_br.x - frac_tl.x) / (double(pix_br.x) - double(pix_tl.x));
-		double y_scale = (frac_br.y - frac_tl.y) / (double(pix_br.y) - double(pix_tl.y));
-
-		double y_pos = frac_tl.y;
-
-		int y_offset = 0;
-		int row_size = ScreenWidth();
-
-		int x, y;
-
-		__m256d _a, _b, _two, _four, _mask1;
-		__m256d _zr, _zi, _zr2, _zi2, _cr, _ci;
-		__m256d _x_pos_offsets, _x_pos, _x_scale, _x_jump;
-		__m256i _one, _c, _n, _iterations, _mask2;
-
-		_one = _mm256_set1_epi64x(1);
-		_two = _mm256_set1_pd(2.0);
-		_four = _mm256_set1_pd(4.0);
-		_iterations = _mm256_set1_epi64x(iterations);
-
-		_x_scale = _mm256_set1_pd(x_scale);
-		_x_jump = _mm256_set1_pd(x_scale * 4);
-		_x_pos_offsets = _mm256_set_pd(0, 1, 2, 3);
-		_x_pos_offsets = _mm256_mul_pd(_x_pos_offsets, _x_scale);
-
-
-		for (y = pix_tl.y; y < pix_br.y; y++)
-		{
-			// Reset x_position
-			_a = _mm256_set1_pd(frac_tl.x);
-			_x_pos = _mm256_add_pd(_a, _x_pos_offsets);
-
-			_ci = _mm256_set1_pd(y_pos);
-
-			for (x = pix_tl.x; x < pix_br.x; x += 4)
-			{
-				_cr = _x_pos;
-				_zr = _mm256_setzero_pd();
-				_zi = _mm256_setzero_pd();
-				_n = _mm256_setzero_si256();
-
-
-			repeat:
-				_zr2 = _mm256_mul_pd(_zr, _zr);
-				_zi2 = _mm256_mul_pd(_zi, _zi);
-				_a = _mm256_sub_pd(_zr2, _zi2);
-				_a = _mm256_add_pd(_a, _cr);
-				_b = _mm256_mul_pd(_zr, _zi);
-				_b = _mm256_fmadd_pd(_b, _two, _ci);
-				_zr = _a;
-				_zi = _b;
-				_a = _mm256_add_pd(_zr2, _zi2);
-				_mask1 = _mm256_cmp_pd(_a, _four, _CMP_LT_OQ);
-				_mask2 = _mm256_cmpgt_epi64(_iterations, _n);
-				_mask2 = _mm256_and_si256(_mask2, _mm256_castpd_si256(_mask1));
-				_c = _mm256_and_si256(_one, _mask2); // Zero out ones where n < iterations													
-				_n = _mm256_add_epi64(_n, _c); // n++ Increase all n
-				if (_mm256_movemask_pd(_mm256_castsi256_pd(_mask2)) > 0)
-					goto repeat;
-
-				pFractal[y_offset + x + 0] = int(_n.m256i_i64[3]);
-				pFractal[y_offset + x + 1] = int(_n.m256i_i64[2]);
-				pFractal[y_offset + x + 2] = int(_n.m256i_i64[1]);
-				pFractal[y_offset + x + 3] = int(_n.m256i_i64[0]);
-				_x_pos = _mm256_add_pd(_x_pos, _x_jump);
-			}
-
-			y_pos += y_scale;
-			y_offset += row_size;
-		}
-	}
-
-	// New method 4 - using built in parallelisation
+	// Using built in parallelisation
 	void CreateFractalParallelization(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations)
 	{
 		const double x_scale = (frac_br.x - frac_tl.x) / (double(pix_br.x) - double(pix_tl.x));
@@ -431,133 +222,12 @@ public:
 			});
 	}
 
-	// Method 5) - Spawn threads No Complex method above
-	void CreateFractalThreads(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations)
-	{
-		int nSectionWidth = (pix_br.x - pix_tl.x) / nMaxThreads;
-		double dFractalWidth = (frac_br.x - frac_tl.x) / double(nMaxThreads);
-
-		std::thread t[nMaxThreads];
-
-		for (size_t i = 0; i < nMaxThreads; i++)
-			t[i] = std::thread(&olcFractalExplorer::CreateFractalNoComplex, this,
-				olc::vi2d(pix_tl.x + nSectionWidth * (i), pix_tl.y),
-				olc::vi2d(pix_tl.x + nSectionWidth * (i + 1), pix_br.y),
-				olc::vd2d(frac_tl.x + dFractalWidth * double(i), frac_tl.y),
-				olc::vd2d(frac_tl.x + dFractalWidth * double(i + 1), frac_br.y),
-				iterations);
-
-		// This is a suspend wait, so no CPU cycles are wasted
-		for (size_t i = 0; i < nMaxThreads; i++)
-			t[i].join();
-
-	}
-
-
-	// Method 6) - Threadpool, keep threads alive and reuse them, reducing setup overhead
-	struct WorkerThread
-	{
-		olc::vi2d pix_tl = { 0,0 };
-		olc::vi2d pix_br = { 0,0 };
-		olc::vd2d frac_tl = { 0,0 };
-		olc::vd2d frac_br = { 0,0 };
-		int iterations = 0;
-		std::condition_variable cvStart;
-		bool alive = true;
-		std::mutex mux;
-		int screen_width = 0;
-		olcFractalExplorer* fractalExplorer = nullptr;
-
-		std::thread thread;
-
-		void Start(const olc::vi2d& ptl, const olc::vi2d& pbr, const olc::vd2d& ftl, const olc::vd2d& fbr, const int it)
-		{
-			pix_tl = ptl;
-			pix_br = pbr;
-			frac_tl = ftl;
-			frac_br = fbr;
-			iterations = it;
-			std::unique_lock<std::mutex> lm(mux);
-			cvStart.notify_one();
-		}
-
-		void CreateFractal()
-		{
-			{
-				std::unique_lock<std::mutex> lm(mux);
-				cvStart.wait(lm);
-			}
-
-			while (alive)
-			{
-				fractalExplorer->CreateFractalNoComplex(pix_tl, pix_br, frac_tl, frac_br, iterations);
-
-				{
-					// Aquire the mutex for the counter during this block
-					std::lock_guard<std::mutex> nWCLock(nWorkerCompleteMutex);
-
-					nWorkerComplete++;
-
-					nWorkerCompleteCV.notify_one();
-				}
-
-				std::unique_lock<std::mutex> lm(mux);
-				cvStart.wait(lm);
-			}
-		}
-	};
-
-	WorkerThread workers[nMaxThreads];
-	static std::mutex nWorkerCompleteMutex;
-
-	static std::condition_variable nWorkerCompleteCV;
-
-	static std::atomic<int> nWorkerComplete;
-
-	void InitialiseThreadPool()
-	{
-		for (int i = 0; i < nMaxThreads; i++)
-		{
-			workers[i].alive = true;
-			workers[i].fractalExplorer = this;
-			workers[i].screen_width = ScreenWidth();
-			workers[i].thread = std::thread(&WorkerThread::CreateFractal, &workers[i]);
-		}
-	}
-
-	void CreateFractalThreadPool(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations)
-	{
-		int nSectionWidth = (pix_br.x - pix_tl.x) / nMaxThreads;
-		double dFractalWidth = (frac_br.x - frac_tl.x) / double(nMaxThreads);
-		nWorkerComplete = 0;
-
-		for (size_t i = 0; i < nMaxThreads; i++)
-			workers[i].Start(
-				olc::vi2d(pix_tl.x + nSectionWidth * i, pix_tl.y), 
-				olc::vi2d(pix_tl.x + nSectionWidth * (i + 1), pix_br.y), 
-				olc::vd2d(frac_tl.x + dFractalWidth * double(i), frac_tl.y), 
-				olc::vd2d(frac_tl.x + dFractalWidth * double(i + 1), frac_br.y), 
-				iterations);
-
-		// This is a busy wait - wasting CPU cycles - what else to do?
-		//while (nWorkerComplete < nMaxThreads) // Wait for all workers to complete
-		//{ }
-		// Instead, use signalling with a condition variable
-		while (1)
-		{
-			std::unique_lock<std::mutex> lk(nWorkerCompleteMutex);
-			nWorkerCompleteCV.wait(lk);
-			if (nWorkerComplete >= nMaxThreads)
-				break;
-		}
-	}
-
 	CircularBuffer<std::chrono::duration<double>> elapseBuffer = CircularBuffer<std::chrono::duration<double>>(40);
 
 	using CreateFractalFunction = void(const olc::vi2d& pix_tl, const olc::vi2d& pix_br, const olc::vd2d& frac_tl, const olc::vd2d& frac_br, const int iterations);
 
 	struct method_s {
-		CreateFractalFunction olcFractalExplorer::* pCreateMethod;
+		CreateFractalFunction FractalFramework::* pCreateMethod;
 		string description;
 	};
 
@@ -602,10 +272,6 @@ public:
 		// Handle User Input
 		if (GetKey(olc::K1).bPressed) nMode = 0;
 		if (GetKey(olc::K2).bPressed) nMode = 1;
-		if (GetKey(olc::K3).bPressed) nMode = 2;
-		if (GetKey(olc::K4).bPressed) nMode = 3;
-		if (GetKey(olc::K5).bPressed) nMode = 4;
-		if (GetKey(olc::K6).bPressed) nMode = 5;
 
 		if (GetKey(olc::UP).bPressed) nIterations += 64;
 		if (GetKey(olc::DOWN).bPressed) nIterations -= 64;
@@ -674,36 +340,15 @@ public:
 	}
 };
 
-std::atomic<int> olcFractalExplorer::nWorkerComplete = 0;
-std::mutex olcFractalExplorer::nWorkerCompleteMutex;
-std::condition_variable olcFractalExplorer::nWorkerCompleteCV;
-
-
-const olcFractalExplorer::method_s olcFractalExplorer::Methods[]
+const FractalFramework::method_s FractalFramework::Methods[]
 =
 {
 	{
-		&olcFractalExplorer::CreateFractalBasic,
-		"Naive Method"
-	},
-	{
-		&olcFractalExplorer::CreateFractalPreCalculate,
-		"Precalculate Method"
-	},
-	{
-		&olcFractalExplorer::CreateFractalNoComplex,
-		"Hand-coded Complex Method"
-	},
-	{
-		&olcFractalExplorer::CreateFractalOpenMP,
+		&FractalFramework::CreateFractalOpenMP,
 		"OpenMP parallel for"
 	},
 	{
-		&olcFractalExplorer::CreateFractalThreadPool,
-		"ThreadPool Method"
-	},
-	{
-		&olcFractalExplorer::CreateFractalParallelization,
+		&FractalFramework::CreateFractalParallelization,
 		"parallel_for Method"
 	},
 };
@@ -711,7 +356,7 @@ const olcFractalExplorer::method_s olcFractalExplorer::Methods[]
 
 int main()
 {
-	olcFractalExplorer demo;
+	FractalFramework demo;
 	if (demo.Construct(1280, 720, 1, 1, false, false))
 		demo.Start();
 	return 0;
