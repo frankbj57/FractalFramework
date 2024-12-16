c769dba622c31f2ca00292658e058d86debb1bec
Frank Bodholdt Jakobsen <frankbj@bbsyd.dk>
2024-12-16 18:22:57 +0100

    Add MinGW build command

--- FractalFramework/build.sh
+++ FractalFramework/build.sh
@@ -1,1 +1,8 @@
-g++ -fopenmp -std=c++17 -O2 -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -o FractalFramework.exe FractalFramework.cpp 
+g++ -o FractalFramework.exe FractalFramework.cpp -Wall -fopenmp -std=c++17 -O3 -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static  
+
+
+# MinGW
+#g++ -o olcExampleProgram.exe olcExampleProgram.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17
+
+# Linux
+#g++ -o olcExampleProgram olcExampleProgram.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
